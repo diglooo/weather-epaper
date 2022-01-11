dfe80d8fc09d8933071d073d97b454b6aed39fc3
Davide Di Gloria <62514786+diglooo@users.noreply.github.com>
2022-01-11 21:56:55 +0100

    Add files via upload

--- settings.h
+++ settings.h
@@ -54,9 +54,9 @@ Chinese Simplified -> zh_cn, Chinese Traditional -> zh_tw.
 */
 String OPEN_WEATHER_MAP_LANGUAGE = "it";
 
-#define UTC_OFFSET + 1
-struct dstRule StartRule = {"CEST", Last, Sun, Mar, 2, 3600}; // Central European Summer Time = UTC/GMT +2 hours
-struct dstRule EndRule = {"CET", Last, Sun, Oct, 2, 0};       // Central European Time = UTC/GMT +1 hour
+#define UTC_OFFSET +1
+//struct dstRule StartRule = {"CEST", Last, Sun, Mar, 2, 3600}; // Central European Summer Time = UTC/GMT +2 hours
+//struct dstRule EndRule = {"CET", Last, Sun, Oct, 2, 0};       // Central European Time = UTC/GMT +1 hour
 
 
 // values in metric or imperial system?
@@ -64,13 +64,6 @@ bool IS_METRIC = true;
 
 // Number of forecasts to obtain
 #define MAX_FORECASTS 4
-
-// change for different ntp (time servers)
-#define NTP_SERVERS "0.it.pool.ntp.org", "time.nist.gov", "pool.ntp.org"
-
-// August 1st, 2018
-#define NTP_MIN_VALID_EPOCH 1533081600
-#define NTP_SYNC_TIMEOUT_SECONDS 10
 
 /***************************
  * End Settings
